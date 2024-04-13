c242a6a36c02c4df58ed35d8834ca9083b648e61
Salmen Hichri <salmen.hichri@gmail.com>
2024-04-13 16:13:16 +0100

    Added logic for display of multiple exceptions in exception box

--- packages/js/core/src/ui/ExceptionsBox/create.ts
+++ packages/js/core/src/ui/ExceptionsBox/create.ts
@@ -1,14 +1,32 @@
 import {DomCreator} from '../../types/dom/DomCreator';
-import {ExceptionsBoxProps} from './props';
 
 export const className = 'nlux-comp-exp_box';
 
-export const createExceptionsBoxDom: DomCreator<ExceptionsBoxProps> = (props) => {
+export const createExceptionsBoxDom: DomCreator<void> = () => {
     const exceptionsBox = document.createElement('div');
     exceptionsBox.classList.add(className);
-    exceptionsBox.appendChild(
-        document.createTextNode(props.message),
-    );
 
     return exceptionsBox;
+};
+
+export const createExceptionItemDom: DomCreator<{
+    message: string;
+    ref?: string;
+}> = ({ref, message}) => {
+    const exception = document.createElement('div');
+    exception.classList.add('nlux-comp-exp_itm');
+
+    if (ref) {
+        const refElement = document.createElement('span');
+        refElement.classList.add('nlux-comp-exp_itm_ref');
+        refElement.append(document.createTextNode(ref));
+        exception.append(refElement);
+    }
+
+    const messageElement = document.createElement('span');
+    messageElement.classList.add('nlux-comp-exp_itm_msg');
+    messageElement.append(document.createTextNode(message));
+    exception.append(messageElement);
+
+    return exception;
 };
