0ba3b5192b5103f6b336548919c9b3586449fd7d
Salmen Hichri <salmen.hichri@gmail.com>
2024-04-16 15:14:52 +0100

    Refactored chat segment events

--- packages/shared/src/types/chatSegment/chatSegment.ts
+++ packages/shared/src/types/chatSegment/chatSegment.ts
@@ -1,4 +1,4 @@
-import {AiStreamedMessage, ChatSegmentAiMessage} from './chatSegmentAiMessage';
+import {ChatSegmentAiMessage} from './chatSegmentAiMessage';
 import {ChatSegmentUserMessage} from './chatSegmentUserMessage';
 
 /**
@@ -69,42 +69,3 @@ export type ChatSegmentEvent =
     'aiMessageStreamed' |
     'complete' |
     'error';
-
-export type ChatSegmentEventsMap<MessageType> = {
-    userMessageReceived: UserMessageReceivedCallback;
-    aiMessageReceived: AiMessageReceivedCallback<MessageType>;
-    aiMessageStreamStarted: AiMessageStreamStartedCallback;
-    aiChunkReceived: AiMessageChunkReceivedCallback;
-    aiMessageStreamed: AiMessageStreamedCallback;
-    complete: ChatSegmentCompleteCallback<MessageType>;
-    error: ChatSegmentErrorCallback;
-};
-
-export type UserMessageReceivedCallback = (userMessage: ChatSegmentUserMessage) => void;
-
-export type AiMessageReceivedCallback<MessageType> = (aiMessage: ChatSegmentAiMessage<MessageType> & {
-    status: 'complete';
-    content: MessageType;
-}) => void;
-
-export type AiMessageStreamStartedCallback = (aiMessage: AiStreamedMessage & {
-    status: 'streaming';
-}) => void;
-
-export type AiMessageStreamedCallback = (aiMessage: AiStreamedMessage & {
-    status: 'complete';
-}) => void;
-
-export type AiMessageChunkReceivedCallback = (
-    messageId: string,
-    chunk: string,
-) => void;
-
-export type ChatSegmentCompleteCallback<MessageType> = (
-    updatedChatSegment: ChatSegment<MessageType>,
-) => void;
-
-export type ChatSegmentErrorCallback = (
-    error: Error,
-    relatedMessageId?: string,
-) => void;
