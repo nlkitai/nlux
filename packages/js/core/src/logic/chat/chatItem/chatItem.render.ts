ca317c7d739cc48b45978b9b029a255324f26705
Salmen Hichri <salmen.hichri@gmail.com>
2024-04-18 22:13:10 +0100

    Fixed streamed for vanilla JS

--- packages/js/core/src/logic/chat/chatItem/chatItem.render.ts
+++ packages/js/core/src/logic/chat/chatItem/chatItem.render.ts
@@ -1,5 +1,6 @@
 import {createChatItemDom} from '../../../../../../shared/src/ui/ChatItem/create';
 import {CompRenderer} from '../../../types/comp';
+import {getElement} from '../../../utils/dom/getElement';
 import {CompChatItemActions, CompChatItemElements, CompChatItemEvents, CompChatItemProps} from './chatItem.types';
 
 export const renderChatItem: CompRenderer<
@@ -12,6 +13,8 @@ export const renderChatItem: CompRenderer<
     const root = createChatItemDom(props.domProps);
     appendToRoot(root);
 
+    const messageContainer = getElement(root, '.nlux-comp-msg');
+
     return {
         elements: {
             chatItemContainer: root,
@@ -19,6 +22,13 @@ export const renderChatItem: CompRenderer<
         actions: {
             focus: () => {
                 root.focus();
+            },
+            processStreamedChunk: (chunk: string) => {
+                if (messageContainer) {
+                    messageContainer.append(
+                        document.createTextNode(chunk),
+                    );
+                }
             },
         },
         onDestroy: () => {
