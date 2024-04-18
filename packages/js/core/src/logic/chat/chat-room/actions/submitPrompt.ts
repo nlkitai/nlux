656932715d16fad8f0808e185f122d12db137499
Salmen Hichri <salmen.hichri@gmail.com>
2024-04-18 12:36:59 +0100

    Updated submitPrompt

--- packages/js/core/src/logic/chat/chat-room/actions/submitPrompt.ts
+++ packages/js/core/src/logic/chat/chat-room/actions/submitPrompt.ts
@@ -28,11 +28,11 @@ export const submitPromptFactory = <AiMsg>({
             // Disable prompt while sending message
             //
             const currentPromptBoxProps = promptBoxInstance.getProp('domCompProps');
-            promptBoxInstance.setDomProps({
-                ...currentPromptBoxProps,
-                status: 'submitting',
-            });
+            promptBoxInstance.setDomProps({...currentPromptBoxProps, status: 'submitting'});
 
+            //
+            // Build request and submit prompt
+            //
             const extras: ChatAdapterExtras<AiMsg> = {
                 aiChatProps: context.aiChatProps,
                 conversationHistory: conversation.getConversationContentForAdapter(
@@ -47,6 +47,18 @@ export const submitPromptFactory = <AiMsg>({
                 new Date(),
             );
 
+            const message = conversation.getMessageById(inMessageId);
+            if (!message) {
+                conversation.removeMessage(outMessageId);
+                warn(`Message with id ${inMessageId} not found`);
+                resetPromptBox(false);
+                return;
+            }
+
+            //
+            // Listen to observable events
+            // Always listen to error event
+            //
             result.observable.on('error', (error) => {
                 conversation.removeMessage(outMessageId);
                 conversation.removeMessage(inMessageId);
@@ -59,40 +71,41 @@ export const submitPromptFactory = <AiMsg>({
                 });
             });
 
-            const message = conversation.getMessageById(inMessageId);
-            if (!message) {
-                conversation.removeMessage(outMessageId);
-                throw new Error(`Message with id ${inMessageId} not found`);
-            }
+            // When streaming or fet is complete, update conversation content and trigger messageReceived event
+            const messageContentCompleteHandler = () => {
+                if (message.content) {
+                    // Only add user message to conversation content (used for history, and not displayed)
+                    // if the message was sent successfully and a response was received.
+                    conversation.updateConversationContent({role: 'user', message: messageToSend});
+                    conversation.updateConversationContent(
+                        {role: 'ai', message: message.content as any},
+                    );
+
+                    context.emit('messageReceived', message.content as any);
+                }
+            };
 
             if (result.dataTransferMode === 'fetch') {
+                //
+                // In fetch mode — Listen to aiMessageReceived event and complete event
+                //
                 result.observable.on('aiMessageReceived', (aiMessage) => {
                     message.setContent(aiMessage.content);
-                    conversation.updateConversationContent({role: 'ai', message: aiMessage.content});
-                    context.emit('messageReceived', aiMessage.content);
-                });
-
-                result.observable.on('complete', () => {
+                    messageContentCompleteHandler();
                     resetPromptBox(true);
                 });
             } else {
+                //
+                // In stream mode — Listen to aiChunkReceived event and complete event
+                //
                 result.observable.on('aiChunkReceived', (aiMessageChunk) => {
                     message.appendContent(aiMessageChunk);
                 });
 
                 result.observable.on('complete', () => {
                     message.commitContent();
+                    messageContentCompleteHandler();
                     resetPromptBox(true);
-
-                    if (message.content) {
-                        // Only add user message to conversation content (used for history, and not displayed)
-                        // if the message was sent successfully and a response was received.
-                        conversation.updateConversationContent({role: 'user', message: messageToSend});
-                        conversation.updateConversationContent(
-                            {role: 'ai', message: message.content as any},
-                        );
-                        context.emit('messageReceived', message.content as any);
-                    }
                 });
             }
         } catch (error) {
