f879dec838c747c928f82c826a0fe72f2c971798
Salmen Hichri <salmen.hichri@gmail.com>
2024-04-24 09:34:49 -0400

    Simplified StreamContainerComp

--- packages/react/core/src/logic/StreamContainer/StreamContainerComp.tsx
+++ packages/react/core/src/logic/StreamContainer/StreamContainerComp.tsx
@@ -1,4 +1,4 @@
-import {Ref, useEffect, useImperativeHandle, useRef} from 'react';
+import {Ref, useImperativeHandle, useRef} from 'react';
 import {className as compMessageClassName} from '../../../../../shared/src/ui/Message/create';
 import {
     directionClassName as compMessageDirectionClassName,
@@ -7,7 +7,6 @@ import {
     statusClassName as compMessageStatusClassName,
 } from '../../../../../shared/src/ui/Message/utils/applyNewStatusClassName';
 import {StreamContainerImperativeProps, StreamContainerProps} from './props';
-import {streamingDomService} from './streamingDomServive';
 
 export const StreamContainerComp = (
     props: StreamContainerProps,
@@ -15,38 +14,17 @@ export const StreamContainerComp = (
 ) => {
     const streamContainer = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        if (streamContainer.current) {
-            streamContainer.current.appendChild(streamingDomService.getStreamingDomElement(props.uid));
-        } else {
-            streamingDomService.deleteStreamingDomElement(props.uid);
-        }
-    }, [streamContainer.current]);
-
-    useEffect(() => {
-        return () => {
-            streamingDomService.deleteStreamingDomElement(props.uid);
-        };
-    }, []);
-
     useImperativeHandle(ref, () => ({
         streamChunk: (chunk: string) => {
-            const domById = streamingDomService.getStreamingDomElement(props.uid);
-            domById?.append(
-                // TODO — Handle proper streaming
+            streamContainer.current?.append(
+                // TODO - Handle markdown
                 document.createTextNode(chunk),
             );
         },
     }), []);
 
-    const compDirectionClassName = props.direction
-        ? compMessageDirectionClassName[props.direction]
-        : compMessageDirectionClassName['incoming'];
-
-    const compStatusClassName = props.status
-        ? compMessageStatusClassName[props.status]
-        : compMessageStatusClassName['rendered'];
-
+    const compDirectionClassName = compMessageDirectionClassName['incoming'];
+    const compStatusClassName = compMessageStatusClassName[props.status];
     const className = `${compMessageClassName} ${compStatusClassName} ${compDirectionClassName}`;
 
     return (
