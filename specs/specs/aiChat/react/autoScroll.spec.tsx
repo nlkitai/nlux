2943dab8145bca3e75bd1c9e3f5c05c4b82c22a5
Salmen Hichri <salmen.hichri@gmail.com>
2024-04-24 03:27:45 -0400

    Updated tests for autoScroll

--- specs/specs/aiChat/react/autoScroll.spec.tsx
+++ specs/specs/aiChat/react/autoScroll.spec.tsx
@@ -38,5 +38,71 @@ describe('<AiChat /> + autoScroll', () => {
             // Assert
             expect(conversationContainer?.scrollTo).toHaveBeenCalledWith({behavior: 'instant', top: 50000});
         });
+
+        describe('When a message is being streamed', () => {
+            it('Should scroll to the bottom when the message is streamed', async () => {
+                // Arrange
+                const aiChat = <AiChat adapter={adapterController!.adapter}/>;
+                const {container} = render(aiChat);
+                await waitForRenderCycle();
+                const textArea: HTMLTextAreaElement = container.querySelector('.nlux-comp-prmptBox > textarea')!;
+                const conversationContainer: any = container.querySelector('.nlux-chtRm-cnv-cntr')!;
+                conversationContainer.scrollTo = vi.fn();
+
+                // Act
+                await userEvent.type(textArea, 'Hello{enter}');
+                await waitForRenderCycle();
+                await waitForMdStreamToComplete();
+
+                // Assert
+                expect(conversationContainer?.scrollTo).toHaveBeenCalledWith({behavior: 'instant', top: 50000});
+            });
+        });
+    });
+
+    describe('When autoScroll is set to false', () => {
+        it('Should not scroll to the bottom when the message starts streaming', async () => {
+            // Arrange
+            const aiChat = <AiChat
+                adapter={adapterController!.adapter}
+                conversationOptions={{autoScroll: false}}
+            />;
+            const {container} = render(aiChat);
+            await waitForRenderCycle();
+            const textArea: HTMLTextAreaElement = container.querySelector('.nlux-comp-prmptBox > textarea')!;
+            const conversationContainer: any = container.querySelector('.nlux-chtRm-cnv-cntr')!;
+            conversationContainer.scrollTo = vi.fn();
+
+            // Act
+            await userEvent.type(textArea, 'Hello{enter}');
+            await waitForRenderCycle();
+            await waitForMdStreamToComplete();
+
+            // Assert
+            expect(conversationContainer?.scrollTo).not.toHaveBeenCalled();
+        });
+
+        describe('When a message is being streamed', () => {
+            it('Should not scroll to the bottom when the message is being streamed', async () => {
+                // Arrange
+                const aiChat = <AiChat
+                    adapter={adapterController!.adapter}
+                    conversationOptions={{autoScroll: false}}
+                />;
+                const {container} = render(aiChat);
+                await waitForRenderCycle();
+                const textArea: HTMLTextAreaElement = container.querySelector('.nlux-comp-prmptBox > textarea')!;
+                const conversationContainer: any = container.querySelector('.nlux-chtRm-cnv-cntr')!;
+                conversationContainer.scrollTo = vi.fn();
+
+                // Act
+                await userEvent.type(textArea, 'Hello{enter}');
+                await waitForRenderCycle();
+                await waitForMdStreamToComplete();
+
+                // Assert
+                expect(conversationContainer?.scrollTo).not.toHaveBeenCalled();
+            });
+        });
     });
 });
