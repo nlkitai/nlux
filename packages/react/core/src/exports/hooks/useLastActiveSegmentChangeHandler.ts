6ede662048ada9ccf5dac23490fda76a0883f856
Salmen Hichri <salmen.hichri@gmail.com>
2024-04-24 15:46:31 -0400

    Improved scroll on markdown rendering

--- packages/react/core/src/exports/hooks/useLastActiveSegmentChangeHandler.ts
+++ packages/react/core/src/exports/hooks/useLastActiveSegmentChangeHandler.ts
@@ -1,11 +1,13 @@
 import {MutableRefObject, useCallback} from 'react';
 import {AutoScrollController} from '../../../../../shared/src/interactions/autoScroll/type';
 
+type LastActiveSegmentData = {uid: string; div: HTMLDivElement};
+
 export const useLastActiveSegmentChangeHandler = <AiMsg>(
     autoScrollController: AutoScrollController | undefined,
     lastActiveSegmentIdRef: MutableRefObject<string | undefined>,
 ) => {
-    return useCallback((data: {uid: string; div: HTMLDivElement} | undefined) => {
+    return useCallback((data: LastActiveSegmentData | undefined) => {
         if (!autoScrollController) {
             return;
         }
@@ -13,10 +15,6 @@ export const useLastActiveSegmentChangeHandler = <AiMsg>(
         if (data) {
             lastActiveSegmentIdRef.current = data.uid;
             autoScrollController.handleNewChatSegmentAdded(data.uid, data.div);
-        } else {
-            if (lastActiveSegmentIdRef.current) {
-                autoScrollController.handleChatSegmentRemoved(lastActiveSegmentIdRef.current);
-            }
         }
     }, [autoScrollController]);
 };
