66c878b9e364f7c82eb126fb2995e022994fd3b1
Salmen Hichri <salmen.hichri@gmail.com>
2024-05-13 15:54:23 +0100

    Updated message received callback

--- specs/specs/aiChat/events/js/messageReceived-fetchAdapter.spec.ts
+++ specs/specs/aiChat/events/js/messageReceived-fetchAdapter.spec.ts
@@ -50,4 +50,49 @@ describe('createAiChat() + fetch adapter + events + messageReceived', () => {
             expect(messageReceivedCallback).toHaveBeenCalledOnce();
         });
     });
+
+    describe('When the callback updates between messages', () => {
+        it('It should trigger the messageReceived event with the new callback', async () => {
+            // Arrange
+            const messageReceivedCallback1 = vi.fn();
+            const messageReceivedCallback2 = vi.fn();
+            aiChat = createAiChat()
+                .withAdapter(adapterController!.adapter)
+                .on('messageReceived', messageReceivedCallback1);
+
+            aiChat.mount(rootElement);
+            await waitForRenderCycle();
+
+            const textArea: HTMLTextAreaElement = rootElement.querySelector('.nlux-comp-prmptBox > textarea')!;
+            await userEvent.type(textArea, 'Hello{enter}');
+
+            // Act
+            adapterController!.resolve('Yo!');
+            await waitForRenderCycle();
+
+            // Assert
+            expect(messageReceivedCallback1).toHaveBeenCalledWith({
+                uid: expect.any(String),
+                message: 'Yo!',
+            });
+
+            // Arrange
+            aiChat.removeEventListener('messageReceived', messageReceivedCallback1);
+            aiChat.on('messageReceived', messageReceivedCallback2);
+
+            // Act
+            await userEvent.type(textArea, 'Bonjour{enter}');
+            await waitForRenderCycle();
+
+            adapterController!.resolve('Salut!');
+            await waitForRenderCycle();
+
+            // Assert
+            expect(messageReceivedCallback1).toHaveBeenCalledOnce();
+            expect(messageReceivedCallback2).toHaveBeenCalledWith({
+                uid: expect.any(String),
+                message: 'Salut!',
+            });
+        });
+    });
 });
--- specs/specs/aiChat/events/react/messageReceived-fetchAdapter.spec.tsx
+++ specs/specs/aiChat/events/react/messageReceived-fetchAdapter.spec.tsx
@@ -71,4 +71,59 @@ describe('<AiChat /> + fetch adapter + events + messageReceived', () => {
             });
         });
     });
+
+    describe('When the callback updates between messages', () => {
+        it('It should trigger the messageReceived event with the new callback', async () => {
+            // Arrange
+            const messageReceivedCallback1 = vi.fn();
+            const messageReceivedCallback2 = vi.fn();
+            const aiChat = (
+                <AiChat
+                    adapter={adapterController!.adapter}
+                    events={{messageReceived: messageReceivedCallback1}}
+                />
+            );
+
+            const {container, rerender} = render(aiChat);
+            await waitForReactRenderCycle();
+
+            const textArea: HTMLTextAreaElement = container.querySelector('.nlux-comp-prmptBox > textarea')!;
+            await userEvent.type(textArea, 'Hello{enter}');
+            await waitForReactRenderCycle();
+
+            // Act
+            adapterController!.resolve('Yo!');
+            await waitForReactRenderCycle();
+
+            // Assert
+            expect(messageReceivedCallback1).toHaveBeenCalledWith({
+                uid: expect.any(String),
+                message: 'Yo!',
+            });
+
+            // Arrange
+            rerender(
+                <AiChat
+                    adapter={adapterController!.adapter}
+                    events={{messageReceived: messageReceivedCallback2}}
+                />,
+            );
+
+            await waitForReactRenderCycle();
+            const textArea2: HTMLTextAreaElement = container.querySelector('.nlux-comp-prmptBox > textarea')!;
+            await userEvent.type(textArea2, 'Bonjour{enter}');
+            await waitForReactRenderCycle();
+
+            // Act
+            adapterController!.resolve('Salut!');
+            await waitForReactRenderCycle();
+
+            // Assert
+            expect(messageReceivedCallback1).toHaveBeenCalledOnce();
+            expect(messageReceivedCallback2).toHaveBeenCalledWith({
+                uid: expect.any(String),
+                message: 'Salut!',
+            });
+        });
+    });
 });
