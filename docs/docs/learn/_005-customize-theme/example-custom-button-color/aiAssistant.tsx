9425b911b51ec34d49029f19a3fef4883f8e34b4
Salmen Hichri <salmen.hichri@gmail.com>
2024-05-28 15:47:17 +0100

    Updated the theme customisation learn page

--- docs/docs/learn/_005-customize-theme/example-custom-button-color/aiAssistant.tsx
+++ docs/docs/learn/_005-customize-theme/example-custom-button-color/aiAssistant.tsx
@@ -1,28 +1,23 @@
-export default `import {useMemo} from 'react';
+export default (colorMode: 'dark' | 'light') => `import {useMemo} from 'react';
 import {AiChat} from '@nlux/react';
 import '@nlux/themes/nova.css';
-import './custom-nova-theme.css';
 import {streamAdapter} from './adapter';
-import {user, assistantCssStyle} from './personas';
+import {personas} from './personas';
+
+// Theme variable overrides are defined in theme-overrides.css
+import './theme-overrides.css';
 
 export default () => {
   const adapter = useMemo(() => streamAdapter, []);
   return (
     <AiChat
-      className="custom-ai-chat-comp"
+      // We can use the 'my-theme' class to increase specificity
+      // and override the default theme variables
+      className="my-theme"
+      conversationOptions={{layout: 'bubbles'}}
+      displayOptions={{colorScheme: 'dark'}}
+      personaOptions={personas}
       adapter={adapter}
-      personaOptions={{
-        assistant: {
-          name: 'iAssistant',
-          avatar: <span style={assistantCssStyle}>🤖</span>,
-          tagline: 'Your Genius AI Assistant'
-        },
-        user
-      }}
-      layoutOptions={{
-        height: 320,
-        maxWidth: 600
-      }}
     />
   );
 };`;
--- docs/docs/learn/_005-customize-theme/example-custom-button-color/custom-nova-theme.tsx
+++ docs/docs/learn/_005-customize-theme/example-custom-button-color/custom-nova-theme.tsx
@@ -1,25 +1,41 @@
-export default `.custom-ai-chat-comp.nluxc-root.nluxc-theme-nova {
-    --nluxc-button-background-color: #1f6adb;
-    --nluxc-button-border-color: #1f6adb;
+export default `.my-theme.nlux-AiChat-root.nlux-theme-nova {
+  /* Override top-level chat room colors */
+  --nlux-ChatRoom--BackgroundColor: #0c0c0e;
+  --nlux-ChatRoom--BorderColor: #24233d;
+  --nlux-ChatRoom-Divider--Color: #24233d;
+  --nlux-ChatRoom--BorderWidth: 2px;
 
-    --nluxc-button-active-background-color: #267df8;
-    --nluxc-button-active-border-color: #267df8;
+  /* Override message bubble colors */
+  --nlux-AiMessage--BackgroundColor: #00BFFF;
+  --nlux-HumanMessage--BackgroundColor: #DC143C;
 
-    --nluxc-button-disabled-background-color: #71849f;
-    --nluxc-button-disabled-border-color: #71849f;
+  /* Override border radius */
+  --nlux-ChatRoom--BorderRadius: 5px;
+  --nlux-ChatItem-Avatar--BorderRadius: 5px;
+  --nlux-ChatItem-Message-BubbleLayout--BorderRadius: 5px;
+  --nlux-PromptInput--BorderRadius: 5px;
+  --nlux-SubmitButton--BorderRadius: 5px;
+  
+  /* Override input colors */
+  --nlux-PromptInput--BackgroundColor: #161620;
+  --nlux-PromptInput-Active--BackgroundColor: #161620;
+  --nlux-PromptInput-Disabled--BackgroundColor: #161620;
+ 
+  /* Override submit button colors */
+  --nlux-SubmitButton--BackgroundColor: #161620;
+  --nlux-SubmitButton-Active--BackgroundColor: #24233d;
+  --nlux-SubmitButton-Disabled--BackgroundColor: #161620;
 
-    --nluxc-message-sent-background-color: #1f6adb;
-    --nluxc-message-sent-border-color: #1f6adb;
+  /* Override icon for the send button */
+  --nlux-send-icon: url('data:image/svg+xml,\\
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" \\
+        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">\\
+      <circle cx="12" cy="12" r="10"/>\\
+      <path d="M16 12l-4-4-4 4M12 16V9"/>\\
+    </svg>\\
+  ');
 
-    --nluxc-message-sent-active-background-color: #267df8;
-    --nluxc-message-sent-active-border-color: #267df8;
-
-    --nluxc-input-border-color: #b6cce1;
-    --nluxc-input-active-border-color: #b6cce1;
-    
-    --nluxc-border-radius-xs: 0;
-    --nluxc-border-radius-s: 0;
-    --nluxc-border-radius: 0;
-    --nluxc-border-radius-m: 0;
-    --nluxc-border-radius-l: 0;
-}`
+  /* Gap between submit button and input */
+  --nlux-Composer--Gap: 8px;
+}
+`;
