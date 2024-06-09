34d6fe6438a51f5774246fb89093cf17788c2b71
Salmen Hichri <salmen.hichri@gmail.com>
2024-06-09 09:00:12 +0100

    Added handling of @shared alias in exported DTS

--- pipeline/utils/rollup/generateDts.ts
+++ pipeline/utils/rollup/generateDts.ts
@@ -1,13 +1,23 @@
 import {RollupOptions} from 'rollup';
 import dts from 'rollup-plugin-dts';
+import alias from '@rollup/plugin-alias';
 
-export const generateDts = (fileName: string, isProduction: boolean, sourceFile = './src/index.ts'): RollupOptions => {
+export const generateDts = (
+    fileName: string,
+    isProduction: boolean,
+    sourceFile = './src/index.ts',
+): RollupOptions => {
     const folder = isProduction ? 'prod' : 'dev';
     const path = `../../../dist/${folder}/${fileName}`;
     return {
         input: sourceFile,
         logLevel: 'silent',
         plugins: [
+            alias({
+                entries: [
+                    {find: /^@shared\/(.*)/, replacement: `../../../packages/shared/src/$1.ts`},
+                ],
+            }),
             dts(),
         ],
         output: {
