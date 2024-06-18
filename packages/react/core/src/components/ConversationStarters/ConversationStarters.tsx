4cebf280a47e4c0d07570eb4f1cbace9f53c6324
somebodyawesome-dev <luckynoob2011830@gmail.com>
2024-06-18 23:08:35 +0100

    added icon component to ConversationStarters component

--- packages/react/core/src/components/ConversationStarters/ConversationStarters.tsx
+++ packages/react/core/src/components/ConversationStarters/ConversationStarters.tsx
@@ -1,20 +1,32 @@
-import {ConversationStartersProps} from './props';
+import { ConversationStarter } from "../../types/conversationStarter";
+import { ConversationStartersProps } from "./props";
 
 export const ConversationStarters = (props: ConversationStartersProps) => {
-    const {onConversationStarterSelected} = props;
-    return (
-        <div className="nlux-comp-conversationStarters">
-            {props.items.map((conversationStarter, index) => (
-                <button
-                    key={index}
-                    className="nlux-comp-conversationStarter"
-                    onClick={() => onConversationStarterSelected(conversationStarter)}
-                >
-                    <span className="nlux-comp-conversationStarter-prompt">
-                        {conversationStarter.prompt}
-                    </span>
-                </button>
-            ))}
-        </div>
-    );
+  const { onConversationStarterSelected } = props;
+  return (
+    <div className="nlux-comp-conversationStarters">
+      {props.items.map((conversationStarter, index) => (
+        <button
+          key={index}
+          className="nlux-comp-conversationStarter"
+          onClick={() => onConversationStarterSelected(conversationStarter)}
+        >
+          <ConversationStarterIcon icon={conversationStarter.icon} />
+          <span className="nlux-comp-conversationStarter-prompt">
+            {conversationStarter.prompt}
+          </span>
+        </button>
+      ))}
+    </div>
+  );
+};
+
+const ConversationStarterIcon = ({
+  icon,
+}: {
+  icon: ConversationStarter["icon"];
+}) => {
+  if (!icon) return null;
+  if (typeof icon === "string") return <img src={icon} width={20} />;
+  return icon;
 };
