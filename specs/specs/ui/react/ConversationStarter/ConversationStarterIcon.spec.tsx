48cd3f533ab3e9da85c09c5fa1d08bbe9bcb91c2
somebodyawesome-dev <luckynoob2011830@gmail.com>
2024-06-20 00:20:53 +0100

    wrote some unit tests for conversation starter components in nlux/react

---
+++ specs/specs/ui/react/ConversationStarter/ConversationStarterIcon.spec.tsx
@@ -0,0 +1,37 @@
+import { ConversationStarterIcon } from "@nlux-dev/react/src/components/ConversationStarters/ConversationStarters";
+import { render } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+
+describe("When conversationStarters is provided", () => {
+  it("Should render the img tag", async () => {
+    // Arrange
+    const component = (
+      <ConversationStarterIcon icon="https://avatars.githubusercontent.com/u/59267562?v=4" />
+    );
+
+    // Act
+    const { getByRole } = render(component);
+
+    // Assert
+    const imgElement = getByRole("img");
+    expect(imgElement).toBeInTheDocument();
+    expect(imgElement).toHaveAttribute(
+      "src",
+      "https://avatars.githubusercontent.com/u/59267562?v=4"
+    );
+    expect(imgElement).toHaveAttribute("width", "20");
+  });
+
+  it("Should render the custom element", async () => {
+    // Arrange
+    const component = <ConversationStarterIcon icon={<div>Custom Icon</div>} />;
+
+    // Act
+    const { getByText } = render(component);
+
+    // Assert
+    const customIconElement = getByText("Custom Icon");
+    expect(customIconElement).toBeInTheDocument();
+    expect(customIconElement).toHaveTextContent("Custom Icon");
+  });
+});
