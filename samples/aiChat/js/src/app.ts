ca576bd15063d3be5d024e20c8b1c11cef718717
somebodyawesome-dev <luckynoob2011830@gmail.com>
2024-06-21 00:08:58 +0100

    added label and icons  to js/samples

--- samples/aiChat/js/src/app.ts
+++ samples/aiChat/js/src/app.ts
@@ -134,6 +134,15 @@ document.addEventListener('DOMContentLoaded', () => {
                 {
                     icon: domElement,
                     prompt: 'How can I assist you today?',
+                    icon: 'https://avatars.githubusercontent.com/u/59267562?v=4',
+                    prompt: 'Write Hello World in Python, C++, and Java.',
+                },
+                {
+                    prompt: 'What can I help you with today? Imagine I am a chatbot.',
+                    label: 'Reverse Roles',
+                },
+                {
+                    prompt: 'How can I assist you today?',
                 },
             ],
         })
