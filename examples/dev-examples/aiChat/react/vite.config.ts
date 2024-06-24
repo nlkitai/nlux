a8e79e9f63a3372478afe7e401624edbffe4f62e
Salmen Hichri <salmen.hichri@gmail.com>
2024-06-24 05:47:17 +0100

    Updated pipeline config

--- examples/dev-examples/aiChat/react/vite.config.ts
+++ examples/dev-examples/aiChat/react/vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react';
 import {getSharedViteConfig} from '../../sharedViteConfig';
 
 export default getSharedViteConfig(
-    '../../..',
+    '../../../..',
     true,
     [react()],
 );
