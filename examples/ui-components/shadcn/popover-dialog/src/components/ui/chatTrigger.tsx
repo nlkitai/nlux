60b0fa668f06b69e3105f2a9397333c80b0fdb48
Mohamed Bashar Touil <luckynoob2011830@gmail.com>
2024-07-08 08:02:25 +0100

    Fix shadcn UI example (#113)
    * corrected --nlux-ChatRoom--Padding values to match figma
    
    * sync shadcn example with demo example

--- examples/ui-components/shadcn/popover-dialog/src/components/ui/chatTrigger.tsx
+++ examples/ui-components/shadcn/popover-dialog/src/components/ui/chatTrigger.tsx
@@ -1,22 +1,29 @@
-import {Button} from '@/components/ui/button.tsx';
-import {ChatBubbleIcon} from '@radix-ui/react-icons';
-import {ChevronDown} from 'lucide-react';
+import { Button } from "@/components/ui/button.tsx";
+import { ChatBubbleIcon } from "@radix-ui/react-icons";
+import { ChevronDown } from "lucide-react";
 
-export const ChatTrigger = ({ pos, mode, isChatOpen, setIsChatOpen } : {
-    pos: "bottom" | "center",
-    mode: "popover" | "drawer" | "dialog",
-    isChatOpen: boolean,
-    setIsChatOpen: (val: boolean) => void
+export const ChatTrigger = ({
+  pos,
+  mode,
+  isChatOpen,
+  setIsChatOpen,
+}: {
+  pos: "bottom" | "center";
+  mode: "popover" | "drawer" | "dialog";
+  isChatOpen: boolean;
+  setIsChatOpen: (val: boolean) => void;
 }) => (
-    <Button
-        size={mode === "popover" ? "icon" : "lg"}
-        className={`${pos === 'bottom' ? 'absolute right-4 bottom-5' : 'mx-auto'} rounded-full z-40`}
-        onClick={() => setIsChatOpen(!isChatOpen)}
-    >
-        {!isChatOpen ? (
-            <ChatBubbleIcon className="h-4 w-4" />
-        ) : (
-            <ChevronDown className="h-4 w-4" />
-        )}
-    </Button>
+  <Button
+    size={mode === "popover" ? "icon" : "lg"}
+    className={`${
+      pos === "bottom" ? "w-14 h-14" : "mx-auto"
+    } rounded-full z-40`}
+    onClick={() => setIsChatOpen(!isChatOpen)}
+  >
+    {!isChatOpen ? (
+      <ChatBubbleIcon className="h-4 w-4" />
+    ) : (
+      <ChevronDown className="h-4 w-4" />
+    )}
+  </Button>
 );
