f00974e2525e792fd1699347ea840775bbf936d9
somebodyawesome-dev <luckynoob2011830@gmail.com>
2024-07-11 00:38:06 +0100

    added react/bedrock adapter

---
+++ packages/react/bedrock/src/index.ts
@@ -0,0 +1,10 @@
+export type {
+  ChatAdapter,
+  StandardChatAdapter,
+  DataTransferMode,
+  ChatAdapterOptions,
+  ChatAdapterBuilder,
+  StreamingAdapterObserver,
+} from "@nlux/bedrock";
+
+export { useChatAdapter } from "./hooks/useChatAdapter";
