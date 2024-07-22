b137dfe91d24d9d5864543618645e1aee25f9f45
somebodyawesome-dev <luckynoob2011830@gmail.com>
2024-07-22 23:16:01 +0100

    added bedrock adapter docs

---
+++ docs/docs/learn/003-adapters/004-bedrock/examples/aiAssistant-default.tsx
@@ -0,0 +1,31 @@
+export default `import {AiChat} from '@nlux/react';
+import {useChatAdapter} from '@nlux/langchain-react';
+import '@nlux/themes/nova.css';
+
+export default () => {
+  // LangServe adapter that connects to a demo LangChain Runnable API
+  const adapter = useChatAdapter({
+    url: 'https://pynlux.api.nlkit.com/pirate-speak'
+  });
+
+  return (
+    <AiChat
+      adapter={adapter}
+      personaOptions={{
+        assistant: {
+          name: 'Feather',
+          avatar: 'https://docs.nlkit.com/nlux/images/personas/feather.png',
+          tagline: 'Yer AI First Mate!'
+        },
+        user: {
+          name: 'Alex',
+          avatar: 'https://docs.nlkit.com/nlux/images/personas/alex.png'
+        }
+      }}
+      layoutOptions={{
+        height: 320,
+        maxWidth: 600
+      }}
+    />
+  );
+};`;
