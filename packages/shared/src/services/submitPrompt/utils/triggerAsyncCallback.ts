c16ee891b226247fd4cf15807ff2967866e9db4a
Salmen Hichri <salmen.hichri@gmail.com>
2024-08-08 01:40:39 +0100

    Updated triggerAsyncCallback

--- packages/shared/src/services/submitPrompt/utils/triggerAsyncCallback.ts
+++ packages/shared/src/services/submitPrompt/utils/triggerAsyncCallback.ts
@@ -3,7 +3,7 @@ import {CallbackFunction} from '../../../types/callbackFunction';
 const isHighPerfJsEngine = navigator?.userAgent?.includes('Safari');
 
 // TODO: To be replaced with a pure React implementation for submitPrompt that relies on React rendering cycle
-const defaultAsyncDelay: 1 | 10 = isHighPerfJsEngine ? 10 : 1; // Efficient async scheduling for Safari
+const defaultAsyncDelay: 1 | 100 = isHighPerfJsEngine ? 100 : 1; // Efficient async scheduling for Safari
 
 export const triggerAsyncCallback = (trigger: CallbackFunction, delay: number = defaultAsyncDelay) => {
     setTimeout(() => {
