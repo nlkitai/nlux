e8cd181e970791aeb9b6e6fc76f5cc10cbca7651
Dmitriy <dchenk@users.noreply.github.com>
2024-08-15 16:26:35 -0500

    Fix Next.js error
    In Node.js `navigator` isn't defined so I was getting ReferenceError: navigator is not defined.

--- packages/shared/src/services/submitPrompt/utils/triggerAsyncCallback.ts
+++ packages/shared/src/services/submitPrompt/utils/triggerAsyncCallback.ts
@@ -1,6 +1,6 @@
 import {CallbackFunction} from '../../../types/callbackFunction';
 
-const isHighPerfJsEngine = navigator?.userAgent?.includes('Safari');
+const isHighPerfJsEngine = typeof navigator !== 'undefined' && navigator?.userAgent?.includes('Safari');
 
 // TODO: To be replaced with a pure React implementation for submitPrompt that relies on React rendering cycle
 const defaultAsyncDelay: 1 | 100 = isHighPerfJsEngine ? 100 : 1; // Efficient async scheduling for Safari
